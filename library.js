7b71739d7c8d03c786dba4ecf77432971a2dff49
a5mith <aaronsmith91@sky.com>
2014-04-16 02:41:19 +0100

    test

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
 	"use strict";
 
 	var SoundCloud = {},
-		embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1"></iframe>';
+		embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>';
 
 
 	SoundCloud.parse = function(postContent, callback) {
@@ -13,3 +13,5 @@
 
 	module.exports = SoundCloud;
 }(module));
+
+                <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/29817339&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>
