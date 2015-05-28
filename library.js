900bd3b9e16d493a5785d092b834af8009adaece
Aaron Smith <a5mith@users.noreply.github.com>
2015-05-28 23:56:53 +0100

    Update to support soundcloud sets.

--- library.js
+++ library.js
@@ -1,17 +1,26 @@
 (function(module) {
-	"use strict";
+    "use strict";
+    var Soundcloud = {},
+        embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/$2&amp;show_artwork=true"></iframe>',
+        embeds = '<iframe width="100%" height="410" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1/sets/$2&amp;show_artwork=true"></iframe>'
+    var	embedtrack = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/([\w\-_]+)">.+<\/a>/g;
+    var	embedset = /<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?([\w\-_]+)\/sets\/([\w\-_]+)">.+<\/a>/g;
 
-	var SoundCloud = {},
-		embed = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/$1&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>';
 
+    Soundcloud.parse = function(data, callback) {
 
-	SoundCloud.parse = function(postContent, callback) {
-		// modified from http://stackoverflow.com/questions/7168987/
-		postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:soundcloud\.com)\/?(.+)">.+<\/a>/g, embed);
-		callback(null, postContent);
-	};
+        if (!data || !data.postData || !data.postData.content) {
+            return callback(null, data);
+        }
+        if (data.postData.content.match(embedtrack)) {
+            data.postData.content = data.postData.content.replace(embedtrack, embed);
+        }
+        if (data.postData.content.match(embedset)) {
+            data.postData.content = data.postData.content.replace(embedset, embeds);
+        }
 
-	module.exports = SoundCloud;
+        callback(null, data);
+    };
+
+    module.exports = Soundcloud;
 }(module));
-
-                <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/29817339&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=true"></iframe>
